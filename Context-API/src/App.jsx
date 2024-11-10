7bd8e8133613489ad6f7e92f62b1c50fc6d8b7dd
Antim <antimsarker0@gmail.com>
2024-11-10 16:37:27 +0600

    login and profile component added

--- Context-API/src/App.jsx
+++ Context-API/src/App.jsx
@@ -1,13 +1,13 @@
-
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
  
 
   return (
-    <>
-     <h1 className='text-black'>React with Chai</h1>
+    <UserContextProvider>
+     <h1 className='text-black text-center mt-10 text-5xl font-semibold '>React with Chai</h1>
       
-    </>
+    </UserContextProvider>
   )
 }
 
---
+++ Context-API/src/components/Login.jsx
@@ -0,0 +1,28 @@
+import {useState,useContext} from 'react';
+import UserContext  from "../context/UserContext"
+
+const Login = () => {
+    const [username,setUsername]=useState('');
+    const [password,setPassword]=useState('');
+    const {setUser}=useContext(UserContext)
+    const handleSubmit=(e)=>{
+           e.preventDefault();
+           setUser({username,password})
+    }
+  return (
+    <div>
+  <h2>Login</h2>
+  <input type="text" placeholder='username'
+      value={username}
+      onChange={(e)=>setUsername(e.target.value)}/>
+
+  <input type="text"
+  value={username}
+  onChange={(e)=>setPassword(e.target.value)}
+   placeholder='password' />  
+    <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
